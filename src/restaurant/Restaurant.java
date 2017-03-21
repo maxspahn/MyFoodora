5ddafacb12f83b7cdef7d2deaa50f9b81751d0ac
maxspahn <maxspahn3@yahoo.de>
2017-03-22 00:40:59 +0100

    now its gonna work

--- src/restaurant/Restaurant.java
+++ src/restaurant/Restaurant.java
@@ -9,6 +9,7 @@ public class Restaurant {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("hello jeremy");
+		System.out.println("ca marche");
 	}
 
 }
