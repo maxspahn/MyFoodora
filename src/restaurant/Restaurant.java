c5649daa7860a244349c952b40bc850216002687
maxspahn <maxspahn3@yahoo.de>
2017-03-22 00:01:11 +0100

    Initial commit, class Restaurant added

---
+++ src/restaurant/Restaurant.java
@@ -0,0 +1,13 @@
+package restaurant;
+
+public class Restaurant {
+
+	public Restaurant() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+
+}
